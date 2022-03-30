7bf75df833e27d246a37767d93dde0a032b62c00
Rafael <aguilera.morales.rafa@gmail.com>
2022-03-30 15:58:01 -0700

    program running

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
-// import files and packages up here
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('../server/data.json')
 
 // create your express server below
-var app;
+
+const app = express();
+app.use(morgan('dev'));
 
 // add your routes and middleware below
+app.get('/data', function (req, res){
+res.json(data);
+});
+
+app.get('/', function(req, res){
+res.status(200).send('all top spots');
+});
+
+app.get('*', function(req, res){
+res.end('Not Found');
+});
 
 // finally export the express application
 module.exports = app;
